d31387fb4b1b89c53bfe8cfd5ae154bffd259661
Pierre Dulac <dulacpier@gmail.com>
2020-08-19 11:09:13 +0200

    Move backport dependency to the setup script

--- setup.py
+++ setup.py
@@ -10,5 +10,10 @@ with io.open("src/flask_sqlalchemy/__init__.py", encoding="utf8") as f:
 setup(
     name="Flask-SQLAlchemy",
     version=version,
-    install_requires=["Flask>=1.0.4", "future_fstrings>=1.2", "SQLAlchemy>=1.2"],
+    install_requires=[
+        "backports.time_perf_counter==0.0.4",
+        "Flask>=1.0.4",
+        "future_fstrings>=1.2",
+        "SQLAlchemy>=1.2",
+    ]
 )
