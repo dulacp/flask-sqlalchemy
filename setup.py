aa302625c8560e98bd08748c73595610aa7f297e
Pierre Dulac <dulacp@google.com>
2021-06-11 10:31:58 +0300

    Pinned SQLAlchemy version to 1.2.x

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     packages=find_packages(where="src"),
     include_package_data=True,
     python_requires=">= 2.7, != 3.0.*, != 3.1.*, != 3.2.*, != 3.3.*, !=3.4.*",
-    install_requires=["Flask>=1.0.4", "SQLAlchemy>=1.2"],
+    install_requires=["Flask>=1.0.4", "SQLAlchemy==1.2.19"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
